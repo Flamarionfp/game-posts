726b097ea1f086a7d24dab9bfdb39fb5f8a489fa
flamarionpinto <flamarionpinto@sou.faccat.br>
2021-11-20 14:07:10 -0300

    Loading até carregar as postagens

--- src/screens/Home/EmptyData.js
+++ src/screens/Home/EmptyData.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 import {Center, DefaultText} from 'styles/globalStyledComponents';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Colors } from 'constants/Colors';
 
 const EmptyData = () => {
   return (
-    <>
-      
-    </>
+    <View>
+      <ActivityIndicator size="large" color={Colors.primaryColor} />
+    </View>
   );
 };
 
