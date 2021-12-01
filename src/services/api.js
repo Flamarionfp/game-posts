bd0c89e5b3a443e07f1a6ff1480d434c15ffb12b
flamarionpinto <flamarionpinto@sou.faccat.br>
2021-11-30 23:26:50 -0300

    porta e ip padrão

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const port = 3333
+const ip = '10.0.2.2'
+
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:3333/',
+  baseURL: `http://${ip}:${port}/`,
 });
 
 export default api;
